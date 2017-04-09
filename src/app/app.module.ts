314cfcde5a87f8265c1460d1aeae8c0beaf02619
CSEugene <cuongnm265@gmail.com>
2017-04-09 11:41:44 +0700

    Sample crud

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,19 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { AngularFireModule } from "angularfire2";
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyC5V9OYfSXu0GclHFSA9BlM9Oj3jyBY0kA",
+  authDomain: "news-776a8.firebaseapp.com",
+  databaseURL: "https://news-776a8.firebaseio.com",
+  projectId: "news-776a8",
+  storageBucket: "news-776a8.appspot.com",
+  messagingSenderId: "508155161033"
+};
 
 @NgModule({
   declarations: [
@@ -14,7 +25,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,7 +36,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
-export class AppModule {}
+export class AppModule { }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController, ActionSheetController } from 'ionic-angular';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,105 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  songs: FirebaseListObservable<any>;
 
+  constructor(public navCtrl: NavController, af: AngularFire, 
+              public alertController: AlertController,
+              public actionSheetController: ActionSheetController) {
+    this.songs = af.database.list('/songs');
   }
 
+  addSong() {
+    let prompt = this.alertController.create({
+      title: 'Song name',
+      message: 'Enter a name for this song',
+      inputs: [
+        {
+          name: 'title',
+          placeholder: 'Title'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            this.songs.push({
+              title: data.title
+            });
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
+  showOptions(songId, songTitle) {
+    let actionSheet = this.actionSheetController.create({
+      title: 'What do you want to do?',
+      buttons: [
+        {
+          text: 'Delete Song',
+          role: 'destructive',
+          handler: () => {
+            this.removeSong(songId);
+          }
+        }, 
+        {
+          text: 'Update Title',
+          handler: () => {
+            this.updateSong(songId, songTitle);
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+
+  removeSong(songId) {
+    this.songs.remove(songId);
+  }
+
+  updateSong(songId, songTitle) {
+    let prompt = this.alertController.create({
+      title: 'Song name',
+      message: 'Update title for this song',
+      inputs: [
+        {
+          name: 'title',
+          placeholder: 'Title',
+          value: songTitle
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            this.songs.update(songId, {
+              title: data.title
+            });
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
 }
