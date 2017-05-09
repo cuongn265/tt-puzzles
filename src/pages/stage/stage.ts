c31b4a4ba46200dc0dba7c783daa770ab50bc100
Thang Le Quoc <silverhair.guy@gmail.com>
2017-05-09 22:18:16 +0700

    bug fixed

--- src/pages/stage/stage.ts
+++ src/pages/stage/stage.ts
@@ -204,9 +204,10 @@ export class StagePage {
       .subscribe(
       (matches: Array<string>) => {
         this.speechList = matches;
-        this.twisterTouched = true;
+        
         loadingSpinner.present().then(() => {
           this.stringComparisonService.returnClosestStringMatch(this.currentTwister.text, this.speechList).then((closestString: string) => {
+            this.twisterTouched = true;
             this.userAnswer = closestString;
             this.stringFormatterService.returnFormattedAnswer(this.currentTwister, this.userAnswer).then((formattedAnswer) => {
               this.formattedAnswer = formattedAnswer;
